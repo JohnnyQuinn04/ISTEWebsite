83a3e8425392fc63c9bf818cd8dc866cb74f4386
ejt7508 <ejt7508@rit.edu>
2024-11-19 15:43:17 -0500

    Changed file structure and file names

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $title; ?></title>
 
-        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="assets/css/styles.css" rel="stylesheet">
     </head>
     <body>
         <header>
