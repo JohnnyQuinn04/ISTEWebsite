4cd78ab5ab358a26046ede8e03c46b3e695bb677
ejt7508 <ejt7508@rit.edu>
2024-11-27 22:37:12 -0500

    Updated styling of nav bar and added custom font

--- assets/inc/nav.inc.php
+++ assets/inc/nav.inc.php
@@ -1,39 +1,42 @@
-<nav>
-    <ul class="navbar">
-        <!-- Contrast Dropdown -->
-        <li class="dropdown">
-            <a href="../../contrast/content.php">Contrast</a>
-            <ul class="dropdown-menu">
-                <li><a href="../../contrast/content.php">Content</a></li>
-                <li><a href="../../contrast/examples.php">Examples</a></li>    
-            </ul>
-        </li>
-        <!-- Repetition Dropdown -->
-        <li class="dropdown">
-            <a href="../../content.php">Repetition</a>
-            <ul class="dropdown-menu">
-                <li><a href="../../content.php">Content</a></li>
-                <li><a href="../../examples.php">Examples</a></li>
-                
-            </ul>
-        </li>
-        <!-- Alignment Dropdown -->
-        <li class="dropdown">
-            <a href="../../content.php">Alignment</a>
-            <ul class="dropdown-menu">
-                <li><a href="../../content.php">Content</a></li>
-                <li><a href="../../examples.php">Examples</a></li>
-                
-            </ul>
-        </li>
-        <!-- For some reason im having trouble with filepaths -->
-        <!-- Proximity Dropdown -->
-        <li class="dropdown">
-            <a href="../../content.php">Proximity</a>
-            <ul class="dropdown-menu">
-                <li><a href="../../content.php">Content</a></li>
-                <li><a href="../../examples.php">Examples</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <a href="../index.php">CRAP Home</a>
+        <ul class="navbar">
+            <!-- Contrast Dropdown -->
+            <li class="dropdown">
+                <a href="../../contrast/content.php">Contrast</a>
+                <ul class="dropdown-menu">
+                    <li><a href="../../contrast/content.php">Content</a></li>
+                    <li><a href="../../contrast/examples.php">Examples</a></li>    
+                </ul>
+            </li>
+            <!-- Repetition Dropdown -->
+            <li class="dropdown">
+                <a href="../../content.php">Repetition</a>
+                <ul class="dropdown-menu">
+                    <li><a href="../../content.php">Content</a></li>
+                    <li><a href="../../examples.php">Examples</a></li>
+                    
+                </ul>
+            </li>
+            <!-- Alignment Dropdown -->
+            <li class="dropdown">
+                <a href="../../content.php">Alignment</a>
+                <ul class="dropdown-menu">
+                    <li><a href="../../content.php">Content</a></li>
+                    <li><a href="../../examples.php">Examples</a></li>
+                    
+                </ul>
+            </li>
+            <!-- For some reason im having trouble with filepaths -->
+            <!-- Proximity Dropdown -->
+            <li class="dropdown">
+                <a href="../../content.php">Proximity</a>
+                <ul class="dropdown-menu">
+                    <li><a href="../../content.php">Content</a></li>
+                    <li><a href="../../examples.php">Examples</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
