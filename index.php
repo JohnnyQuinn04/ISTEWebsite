5f6b8bd7d98bf69d61a15b9ae70c7a3e7499f6d8
ejt7508 <ejt7508@rit.edu>
2024-11-28 15:56:02 -0500

    Added title card for home page

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
-$title = "CRAP Principles: Homepage";
-$path = "assets/css/styles.css";
+    $title = "CRAP Principles: Homepage";
+    $path = "assets/css/styles.css";
 
-require_once("assets/inc/header.inc.php");
+    require_once("assets/inc/header.inc.php");
 
 ?>
+<div class="title">
+    <i class="fa-solid fa-star" style="left: 13%; top: 20px;" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-solid fa-star" style="left: 5%; top: 100px;" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-solid fa-star hide" style="left: 23%; top: 70px" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-regular fa-circle" style="left: 30%; top: 30px;" onclick="this.classList.toggle('fa-beat')"></i>
+    <i class="fa-regular fa-circle" style="left: 16%; top: 130px" onclick="this.classList.toggle('fa-beat')"></i>
+    <div class="homepage">
+        <a href="contrast/content.php" style="color: #FF3A3A;">C</a>
+        <a href="repetition/content.php" style="color: #FFDF3F;">R</a>
+        <a href="alignment/content.php" style="color: #66E04B;">A</a>
+        <a href="proximity/content.php" style="color: #60AFFF;">P</a>
+    </div>
+    <i class="fa-solid fa-star" style="right: 13%; top: 20px;" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-solid fa-star" style="right: 5%; top: 100px;" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-solid fa-star hide" style="right: 23%; top: 70px" onclick="this.classList.toggle('fa-spin')"></i>
+    <i class="fa-regular fa-circle" style="right: 30%; top: 30px;" onclick="this.classList.toggle('fa-beat')"></i>
+    <i class="fa-regular fa-circle" style="right: 16%; top: 130px" onclick="this.classList.toggle('fa-beat')"></i>
+</div>
 
 <main>
     <h1>Welcome to the CRAP Principles Guide</h1>
