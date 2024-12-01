75d5f0b98b666230ec160e34cc9cbc34f6bd8131
ejt7508 <ejt7508@rit.edu>
2024-12-01 12:03:25 -0500

    Added sources page

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,5 +1,6 @@
         <footer>
-            &copy; <?php echo date("Y"); ?> - ISTE240 CRAP Crusaders @ RIT
+            <p>&copy; <?php echo date("Y"); ?> - ISTE240 CRAP Crusaders @ RIT</p>
+            <a href="/~iste240t06/Final/sources.php" <?php if($pageName=="sources") echo 'id="currentHome"'?>>Sources</a>
         </footer>
     </body>
 </html>
---
+++ sources.php
@@ -0,0 +1,35 @@
+<?php
+    $title = "CRAP Principles: Sources";
+    $path = "assets/css/styles.css";
+    // Avoid nav errors with variable names
+    $pageName = "sources";
+    $heading = "";
+    $color = "";
+    require_once("assets/inc/header.inc.php");
+?>
+<main>
+    <div class="sources">
+        <h2>Contrast</h2>
+        <ul>
+            <li><a href="https://elementor.com/blog/contrast-web-design/">https://elementor.com/blog/contrast-web-design/</a></li>
+            <li><a href="https://elements.envato.com/learn/design-contrast-in-web-design">https://elements.envato.com/learn/design-contrast-in-web-design</a></li>
+            <li><a href="https://www.csun.edu/universal-design-center/web-accessibility-criteria-color-contrast">https://www.csun.edu/universal-design-center/web-accessibility-criteria-color-contrast</a></li>
+        </ul>
+        <h2>Repetition</h2>
+        <ul>
+            <li><a href="https://venngage.com/blog/design-principle-repetition/">https://venngage.com/blog/design-principle-repetition/</a></li>
+            <li><a href="https://www.designhill.com/design-blog/the-repetition-principle-of-graphic-design-a-brief-guide/">https://www.designhill.com/design-blog/the-repetition-principle-of-graphic-design-a-brief-guide/</a></li>
+        </ul>
+        <h2>Alignment</h2>
+        <ul>
+            <li><a href="https://ux360.design/alignment-principle-design/">https://ux360.design/alignment-principle-design/</a></li>
+        </ul>
+        <h2>Proximity</h2>
+        <ul>
+            <li><a href="https://mailchimp.com/resources/law-of-proximity">https://mailchimp.com/resources/law-of-proximity</a></li>
+        </ul>
+    </div>
+</main>
+
+<!-- Include Footer -->
+<?php require_once("assets/inc/footer.inc.php"); ?>
