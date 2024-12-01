7b2e3be2996b211a2fdea99a8b35a6308a19098b
ejt7508 <ejt7508@rit.edu>
2024-12-01 10:47:47 -0500

    Fixed error with nav bar, added styling for hovering over links

--- assets/inc/nav.inc.php
+++ assets/inc/nav.inc.php
@@ -4,7 +4,7 @@
     <ul class="navbar">
         <!-- Contrast Dropdown -->
         <li class="dropdown">
-            <p <?php if($heading=="Contrast") echo 'id="currentDir"';?> style="text-decoration-color: <?php echo $color ?>">Contrast</p>
+            <p <?php if($heading=="Contrast") echo 'id="currentDir"';?> style="text-decoration-color: #FF3A3A">Contrast</p>
             <ul class="dropdown-menu">
                 <li><a href="/~iste240t06/Final/contrast/content.php" <?php if($pageName=="contrastContent") echo 'id="current"'?>>Content</a></li>
                 <li><a href="/~iste240t06/Final/contrast/examples.php" <?php if($pageName=="contrastExamples") echo 'id="current"'?>>Examples</a></li>    
@@ -12,7 +12,7 @@
         </li>
         <!-- Repetition Dropdown -->
         <li class="dropdown">
-            <p <?php if($heading=="Repetition") echo 'id="currentDir"';?> style="text-decoration-color: <?php echo $color ?>">Repetition</p>
+            <p <?php if($heading=="Repetition") echo 'id="currentDir"';?> style="text-decoration-color: #FFDF3F">Repetition</p>
             <ul class="dropdown-menu">
                 <li><a href="/~iste240t06/Final/repetition/content.php" <?php if($pageName=="repetitionContent") echo 'id="current"'?>>Content</a></li>
                 <li><a href="/~iste240t06/Final/repetition/examples.php" <?php if($pageName=="repetitionExamples") echo 'id="current"'?>>Examples</a></li>
@@ -20,7 +20,7 @@
         </li>
         <!-- Alignment Dropdown -->
         <li class="dropdown">
-            <p <?php if($heading=="Alignment") echo 'id="currentDir"';?> style="text-decoration-color: <?php echo $color ?>">Alignment</p>
+            <p <?php if($heading=="Alignment") echo 'id="currentDir"';?> style="text-decoration-color: #66E04B">Alignment</p>
             <ul class="dropdown-menu">
                 <li><a href="/~iste240t06/Final/alignment/content.php" <?php if($pageName=="alignmentContent") echo 'id="current"'?>>Content</a></li>
                 <li><a href="/~iste240t06/Final/alignment/examples.php" <?php if($pageName=="alignmentExamples") echo 'id="current"'?>>Examples</a></li>
@@ -28,7 +28,7 @@
         </li>
         <!-- Proximity Dropdown -->
         <li class="dropdown">
-            <p <?php if($heading=="Proximity") echo 'id="currentDir"';?> style="text-decoration-color: <?php echo $color ?>">Proximity</p>
+            <p <?php if($heading=="Proximity") echo 'id="currentDir"';?> style="text-decoration-color: #60AFFF">Proximity</p>
             <ul class="dropdown-menu">
                 <li><a href="/~iste240t06/Final/proximity/content.php" <?php if($pageName=="proximityContent") echo 'id="current"'?>>Content</a></li>
                 <li><a href="/~iste240t06/Final/proximity/examples.php" <?php if($pageName=="proximityExamples") echo 'id="current"'?>>Examples</a></li>
