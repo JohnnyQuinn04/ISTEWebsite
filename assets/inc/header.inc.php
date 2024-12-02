90a9ac4cd271d269ec3c761f7d5e54531c03f6d8
JohnnyQuinn04 <jwq1228@g.rit.edu>
2024-12-02 18:54:19 -0500

    favicon fix

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -7,7 +7,7 @@
     <link href=<?php echo $path ?> rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/~iste240t06/Final/assets/images/favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Coiny&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/64824a9b49.js" crossorigin="anonymous"></script>
 </head>
